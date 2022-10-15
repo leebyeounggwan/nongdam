0c509944191911b087a5d27f1cb088d5080372ef
leebyeounggwan <gwan0622@gmail.com>
2022-10-15 12:12:03 +0900

    first commit

---
+++ src/main/java/com/example/nongdam/NongdamApplication.java
@@ -0,0 +1,13 @@
+package com.example.nongdam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongdamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NongdamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nongdam/NongdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nongdam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NongdamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
