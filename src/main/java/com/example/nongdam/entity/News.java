8bd0c08f449fb9194937e59eb60695fd1703c632
leebyeounggwan <gwan0622@gmail.com>
2022-10-17 19:47:12 +0900

    add : news table, repository 생성

---
+++ src/main/java/com/example/nongdam/entity/News.java
@@ -0,0 +1,25 @@
+package com.example.nongdam.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String url;
+    private String img;
+    private String press;
+    private String time;
+
+}
+
