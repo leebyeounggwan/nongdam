521d96f43d87c481e8010cfd0d11ad109074f7e5
leebyeounggwan <gwan0622@gmail.com>
2022-10-23 20:34:42 +0900

    feat: 카카오 로그인/회원가입 및 일반 로그인 구현

--- src/main/java/com/example/nongdam/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.nongdam.repository;
-
-import com.example.nongdam.entity.Hello;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<Hello, Long> {
-}
